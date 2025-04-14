134d4f2f323bb0ebab35a5f2727856632c4176ea
Anton <moonlayt400@gmail.com>
2025-04-14 14:52:02 +0300

    Database and methods

--- internal/storage/sqllite/sqllite.go
+++ internal/storage/sqllite/sqllite.go
@@ -3,8 +3,9 @@ package sqllite
 import (
 	"database/sql"
 	"fmt"
+	"url-shortener/internal/storage"
 
-	_ "github.com/mattn/go-sqlite3" //driver init
+	"github.com/mattn/go-sqlite3" //driver init
 )
 
 type Storage struct {
@@ -38,3 +39,45 @@ stmt, err := db.Prepare(`
 	return &Storage{db: db}, nil
 }
 
+func (s *Storage) SaveURL(longUrl,alias string) (error) {
+	const op = "storage.sqllite.SaveURL"
+	stmt, err := s.db.Prepare("INSERT INTO url(alias, url) VALUES(?, ?)")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	_, err = stmt.Exec(alias, longUrl)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return  fmt.Errorf("%s: %w", op, storage.ErrUrlExists)
+		}
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqllite.GetURL"
+	var url string
+	err := s.db.QueryRow("SELECT url FROM url WHERE alias = ?", alias).Scan(&url)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", fmt.Errorf("%s: %w", op, storage.ErrUrlNotFound)
+		}
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	return url, nil
+}
+
+
+func (s *Storage) DeleteURL(alias string) (error) {
+	const op = "storage.sqllite.DeleteURL"
+	stmt, err := s.db.Prepare("DELETE FROM url WHERE alias = ?")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	_, err = stmt.Exec(alias)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
